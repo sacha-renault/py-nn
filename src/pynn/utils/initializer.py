51087b7ab6f469ea01d58dacd28bdf53e68d47c7
sacha-renault <sacha35340@gmail.com>
2024-09-23 11:44:33 +0200

    fix (initializer): initializer now returns _tensorArray

--- src/pynn/utils/initializer.py
+++ src/pynn/utils/initializer.py
@@ -1,5 +1,13 @@
+from .. import xp
+
 def initializer_xavier_relu(input_features, output_features) -> float:
-    return (2 / input_features) ** (1 / 2)
+    stddev = (2 / input_features) ** (1 / 2)
+    return xp.random.normal(0, stddev, (input_features + output_features))
 
 def initializer_xavier_tanh(input_features, output_features) -> float:
-    return (1 / input_features) ** (1 / 2)
+    stddev = (1 / input_features) ** (1 / 2)
+    return xp.random.normal(0, stddev, (input_features + output_features))
+
+def he_initializer(input_features, output_features):
+    limit = (6.0 / (input_features + output_features)) ** 0.5
+    return xp.random.rand(input_features, output_features) * limit * 2 - limit 
